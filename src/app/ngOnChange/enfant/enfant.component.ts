9503e14c796a84679a829eacfe2d669a20c5a7f6
amine <el.gorch@hotmail.fr>
2025-09-22 19:56:12 +0200

    declancher NgOnChange avec changement de reference

--- src/app/ngOnChange/enfant/enfant.component.ts
+++ src/app/ngOnChange/enfant/enfant.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-enfant',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './enfant.component.html',
   styleUrl: './enfant.component.css'
 })
-export class EnfantComponent {
+export class EnfantComponent implements OnChanges {
   @Input() valeurRecue!: string;
+  @Input() utilisateur!: { nom: string, age: string };
+  messageChange = false;
+
+  ngOnChanges(changes: SimpleChanges) {
+    const utilisateurChange = changes['utilisateur'];
+
+    console.log('🔁 ngOnChanges déclenché dans enfant', changes);
+
+    if (utilisateurChange && !utilisateurChange.firstChange) {
+      this.messageChange = true;
+
+      setTimeout(() => {
+        this.messageChange = false;
+      }, 2000);
+    }
+  }
 }
